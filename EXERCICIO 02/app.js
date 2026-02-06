141ab72d52f9233ce5b66c4080efa90a4ea9f222
Nephyro <anderson.soare.4245@gmail.com>
2026-02-06 09:51:40 -0300

    finalizando as validações do exercicio 02

--- EXERCICIO 02/app.js
+++ EXERCICIO 02/app.js
@@ -7,6 +7,7 @@ const entradaDeDados = readline.createInterface({
 })
 
 entradaDeDados.question('Por favor digite o seu nome: ', function perguntarNome(nome){ 
+
     let nomeCliente = nome
 
     if(!isNaN(nomeCliente) || nomeCliente ===''){
@@ -15,6 +16,7 @@ entradaDeDados.question('Por favor digite o seu nome: ', function perguntarNome(
          
     }else{
         entradaDeDados.question(`Olá ${nomeCliente}, qual produto deseja comprar: `, function perguntarProduto(produto){
+
             let nomeProduto = produto
 
             if(!isNaN(nomeProduto) || nomeProduto ===''){
@@ -22,16 +24,19 @@ entradaDeDados.question('Por favor digite o seu nome: ', function perguntarNome(
                 entradaDeDados.question(`Olá ${nomeCliente}, qual produto deseja comprar: `, perguntarProduto)
 
             }else{
-                 entradaDeDados.question(`Qual o valor do ${nomeProduto}: `, function perguntarValor(valor){
+                 entradaDeDados.question(`Qual o valor do ${nomeProduto}: R$`, function perguntarValor(valor){
+
                     let valorProduto = valor
 
                     if(isNaN(valorProduto) || valorProduto ===''){
                         console.log(`ERRO: O valor ${valorProduto} digitado não é válido! Por favor utilize apenas números`)
-                        entradaDeDados.question(`Qual o valor do ${nomeProduto}: `, perguntarValor)
+                        entradaDeDados.question(`Qual o valor do ${nomeProduto}: R$`, perguntarValor)
 
                     }else{
                         entradaDeDados.question('Digite a taxa de juros que deverá ser aplicada: ', function perguntarTaxa(taxa){
+
                             let taxaDigitada = taxa
+                            
                             let taxaConvertida = Number(taxaDigitada) / 100
 
                             if(isNaN(taxaDigitada) || taxaDigitada ===''){
@@ -40,60 +45,81 @@ entradaDeDados.question('Por favor digite o seu nome: ', function perguntarNome(
 
                             }else{
                                 entradaDeDados.question('Em quanto tempo deseja pagar? Digite 1 para meses ou 2 para anos: ', function perguntarTempo(escolha){
+
                                     let escolhaDigitada = escolha
+
                                     let montante
+
                                     let quantidadeParcelas
+
                                     let acrescimo
 
-                                    if(isNaN(escolhaDigitada) || escolhaDigitada ===''){
+                                    if(isNaN(escolhaDigitada) || escolhaDigitada ==='' || escolhaDigitada < 1 || escolhaDigitada > 2){
                                         console.log(`ERRO: ${escolhaDigitada} não é válido! Por favor utilize apenas números`)
                                         entradaDeDados.question('Em quanto tempo deseja pagar? Digite 1 para meses ou 2 para anos: ', perguntarTempo)
 
                                     }else if(escolhaDigitada == 1){
                                         entradaDeDados.question('Em quantos mêses você deseja parcelar: ', function perguntarEmMes(escolhaMes){
+
                                             let parcelarEmMes = escolhaMes
+
                                             quantidadeParcelas = Number(parcelarEmMes)
 
                                             montante = Number(valorProduto) * (1 + Number(taxaConvertida)) ** Number(parcelarEmMes)
+
                                             acrescimo = Number(montante) - Number(valorProduto)
 
-                                            console.log(`\n******************* Essencial Alpha *******************
-                                            \nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
-                                            \nA compra do produto ${nomeProduto}, tem um valor de: ${valorProduto}.
-                                            \nA sua compra será parcelada em ${quantidadeParcelas} vezes e o Sr(a) pagará: ${montante}.
-                                            \nO acréscimo realizado ao valor de: ${valorProduto} será de ${acrescimo}.
-                                            \n
-                                            \nMuito obrigado por escolher a Essencial Alpha.
-                                            \n*******************************************************`)
-                                        })
+                                            if (isNaN(parcelarEmMes)){
+                                                console.log(`ERRO: Valor ${parcelarEmMes} não é reconhecido!`)
+                                                entradaDeDados.question('Em quantos mêses você deseja parcelar: ', perguntarEmMes)
 
-                                    }else{
+                                            }else{
+                                                console.log(`\n******************* Essencial Alpha *******************
+                                                    \nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
+                                                    \nA compra do produto ${nomeProduto}, tem um valor de: R$${valorProduto}.
+                                                    \nA sua compra será parcelada em ${quantidadeParcelas} vezes e o Sr(a) pagará um total de: ${montante.toFixed(2)}.
+                                                    \nO acréscimo realizado ao valor de: R$${valorProduto} será de R$${acrescimo.toFixed(2)}.
+                                                    \n
+                                                    \n    Muito obrigado por escolher a Essencial Alpha.
+                                                    \n*******************************************************`)
+                                            }//Fecha condição de meses
+                                        })//Fecha quantidade de parcelas em meses
+
+                                    }else if(escolhaDigitada == 2){
                                         entradaDeDados.question('Em quantos anos você deseja parcelar: ', function perguntarEmAno(escolhaAno){
+
                                             let parcelarEmAnos = escolhaAno
+
                                             let mesesConvertidos = Number(parcelarEmAnos) * 12
+
                                             quantidadeParcelas = Number(mesesConvertidos)
 
                                             let montante = Number(valorProduto) * (1 + Number(taxaConvertida)) ** Number(mesesConvertidos)
+
                                             acrescimo = Number(montante) - Number(valorProduto)
 
-                                            console.log(`\n******************* Essencial Alpha *******************
-                                            \nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
-                                            \nA compra do produto ${nomeProduto}, tem um valor de: ${valorProduto}.
-                                            \nA sua compra será parcelada em ${quantidadeParcelas} vezes e o Sr(a) pagará: ${montante}.
-                                            \nO acréscimo realizado ao valor de: ${valorProduto} será de ${acrescimo}.
-                                            \n
-                                            \nMuito obrigado por escolher a Essencial Alpha.
-                                            \n*******************************************************`)
-                                        })
-                                        
-                                    }
+                                            if (isNaN(parcelarEmAnos)){
+                                                console.log(`ERRO: Valor ${parcelarEmAnos} não é reconhecido!`)
+                                                entradaDeDados.question('Em quantos anos você deseja parcelar: ', perguntarEmAno)
 
-                                })
-                            }
-                        })
-                    }
-                 })
-            }
-        })
-    }
-})
+                                            }else{
+                                                console.log(`\n******************* Essencial Alpha *******************
+                                                    \nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
+                                                    \nA compra do produto ${nomeProduto}, tem um valor de: R$${valorProduto}.
+                                                    \nA sua compra será parcelada em ${quantidadeParcelas} vezes e o Sr(a) pagará um total de: ${montante.toFixed(2)}.
+                                                    \nO acréscimo realizado ao valor de: R$${valorProduto} será de R$${acrescimo.toFixed(2)}.
+                                                    \n
+                                                    \n    Muito obrigado por escolher a Essencial Alpha.
+                                                    \n*******************************************************`)
+                                            }//Fecha condição de anos
+                                        })//Fecha quantidade de parcelas em anos
+                                    }//Fecha o else da escolha em anos
+                                })//Fecha a escolha se será em mês ou ano
+                            }//Fecha a validação da taxa
+                        })//Fecha o valor digitado da taxa
+                    }//Fecha validaçõa do produto
+                 })//Fecha o valor do produto
+            }//Fecha validação do nome do produto
+        })//Fecho o nome do produto
+    }//Fecha validação do nome do cliente
+})//Fecho o nome do cliente
